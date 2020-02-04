9a2f15f873d4ead57b68bc6b1b47d42a31216821
trafalmejo <trafalmejo@gmail.com>
2020-02-04 12:14:43 -0500

    Updates

---
+++ precache-manifest.e8490a57cdf2156fd3b239cf573efd88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4d1a9a6ead87e2ac23419989e6457b",
+    "url": "/react-sketches/dices/index.html"
+  },
+  {
+    "revision": "2a2cce27bc1c23400e4c",
+    "url": "/react-sketches/dices/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b909e2ac13287a8cfa53",
+    "url": "/react-sketches/dices/static/css/main.7b0563db.chunk.css"
+  },
+  {
+    "revision": "2a2cce27bc1c23400e4c",
+    "url": "/react-sketches/dices/static/js/2.126c02fb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-sketches/dices/static/js/2.126c02fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b909e2ac13287a8cfa53",
+    "url": "/react-sketches/dices/static/js/main.7e287b72.chunk.js"
+  },
+  {
+    "revision": "46bda3c66cf039727605",
+    "url": "/react-sketches/dices/static/js/runtime-main.21ade782.js"
+  }
+]);
